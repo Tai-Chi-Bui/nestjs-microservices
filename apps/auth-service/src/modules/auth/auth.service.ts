688e2392b128d8c5bab870d4ec73120388325aaa
Tai C. Bui <tai.c.bui@gmail.com>
2024-11-24 10:15:06 +0700

    Fix bugs for env not read

--- apps/auth-service/src/modules/auth/auth.service.ts
+++ apps/auth-service/src/modules/auth/auth.service.ts
@@ -9,9 +9,9 @@ export class AuthService {
     private readonly usersService: UsersService,
   ) {}
 
-  async validateUser(username: string, pass: string): Promise<any> {
+  async validateUser(username: string, password: string): Promise<any> {
     const user = await this.usersService.findOne(username);
-    if (user && user.password === pass) {
+    if (user && user.password === password) {
       const { password, ...result } = user;
       return result;
     }
@@ -19,7 +19,8 @@ export class AuthService {
   }
 
   async login(user: any) {
-    const payload = { username: user.username, sub: user.id };
+    console.log("user login: ", user)
+    const payload = { username: user.username, password: user.password };
     return {
       access_token: this.jwtService.sign(payload),
     };
