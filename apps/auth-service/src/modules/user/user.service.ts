3d1a11e71dcdd7ab4d0eee673a0bc147767e5ff1
Tai C. Bui <tai.c.bui@gmail.com>
2024-11-24 09:48:02 +0700

    Scafolding for Auth Service

--- apps/auth-service/src/modules/user/user.service.ts
+++ apps/auth-service/src/modules/user/user.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class UsersService {
   private readonly users = [
-    { id: 1, username: 'john', password: 'changeme' },
+    { id: 1, username: 'admin', password: 'admin' },
     { id: 2, username: 'chris', password: 'secret' },
     { id: 3, username: 'maria', password: 'guess' },
   ];
